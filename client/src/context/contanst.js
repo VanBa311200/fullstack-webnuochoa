31e0b01236d01a249b0c406e630fee8b53f2ec41
Nguyen Van Ba <vanba311200@gmail.com>
2022-02-14 15:35:22 +0700

    fixed Heroku H10-App Crashed

--- client/src/context/contanst.js
+++ client/src/context/contanst.js
@@ -1,10 +1,9 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ?
-  'http://localhost:5000'
-  :
-  'https://api-webnuochoa.herokuapp.com'
+export const apiUrl =
+  process.env.NODE_ENV !== "production"
+    ? "https://api-webnuochoa.herokuapp.com"
+    : "https://api-webnuochoa.herokuapp.com";
 
-export const LOCAL_STORAGE_TOKEN_AUTH = 'LOCAL_STORAGE_TOKEN_AUTH'
-export const LOCAL_STORAGE_ITEMSCART = 'LOCAL_STORAGE_ITEMSCART'
+export const LOCAL_STORAGE_TOKEN_AUTH = "LOCAL_STORAGE_TOKEN_AUTH";
+export const LOCAL_STORAGE_ITEMSCART = "LOCAL_STORAGE_ITEMSCART";
 
-export const SET_AUTH = 'SET_AUTH'
-
+export const SET_AUTH = "SET_AUTH";
