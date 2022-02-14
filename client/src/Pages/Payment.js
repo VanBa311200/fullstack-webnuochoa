dcee158a636cca3dc88de85ed4063e904b0b46a3
Nguyen Van Ba <vanba311200@gmail.com>
2022-02-14 18:21:10 +0700

    14/2/2022 test

--- client/src/Pages/Payment.js
+++ client/src/Pages/Payment.js
@@ -1,22 +1,18 @@
-import { Container, Box, Grid } from '@mui/material'
-import React from 'react'
-import Master1 from '../views/Master1'
+import { Container, Grid } from "@mui/material";
+import React from "react";
+import Master1 from "../views/Master1";
 
 const Payment = (props) => {
   return (
     <Master1 {...props}>
       <Container>
-        <Grid contrainer spacing={2} minHeight='70vh'>
-          <Grid item md={8}>
-
-          </Grid>
-          <Grid item md={4}>
-
-          </Grid>
+        <Grid contrainer spacing={2} minHeight="70vh">
+          <Grid item md={8}></Grid>
+          <Grid item md={4}></Grid>
         </Grid>
       </Container>
     </Master1>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
